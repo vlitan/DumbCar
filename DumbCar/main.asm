5e84d5744a10b56d5bc9c9cb7bdc03d79f7906af
VirgilLitan <virgilx13@gmail.com>
2016-12-05 12:33:00 +0200

    working on set_byte macro. pwm (almost) works

--- DumbCar/main.asm
+++ DumbCar/main.asm
@@ -14,6 +14,11 @@
 ; Replace with your application code
 start:
     ;-----setup--------
+	init_timer_0
+	enable_channel_0A
+	enable_channel_0B
+	set_match_0B 100
+	set_match_0A 200
 	;------------------
 	inf:
 		;----infinite loop--
