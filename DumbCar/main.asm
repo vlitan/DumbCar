e9514348afed349510a1df9f6a0e2c548895c888
VirgilLitan <virgilx13@gmail.com>
2016-12-06 12:09:41 +0200

    working pwm demo. 4 channels with pwm

--- DumbCar/main.asm
+++ DumbCar/main.asm
@@ -9,25 +9,38 @@
 .include "m328Pdef.inc"
 .include "utils.inc"
 .include "pwm.inc"
-;======================
+;======defines=========
+.def A=r26
+.def B=r27
+.def C=r28
+.def D=r29
 
 ; Replace with your application code
 start:
     ;-----setup--------
 	init_timer_0
-	set_match_0A 5
-	set_match_0B 10
 	enable_channel_0A
 	enable_channel_0B
 	
 	init_timer_2
-	set_match_2A 200
-	set_match_2B 100
 	enable_channel_2A
 	enable_channel_2B
+
+	ldi A, 0
+	ldi B, 70
+	ldi C, 130
+	ldi D, 190
 	;------------------
 	inf:
-		;----infinite loop--
+		inc A
+		inc B
+		inc C
+		inc D
+		set_match_0A A
+		set_match_0B B
+		set_match_2A C
+		set_match_2B D
+		CALL delay_half_sec ; mult mai putin acum!
 	rjmp inf
 
 ;====include methods===
--- DumbCar/utils.asm
+++ DumbCar/utils.asm
@@ -7,5 +7,18 @@
 
  #ifndef _UTILS_ASM_
  #define _UTILS_ASM_
- ;-----------------
+
+delay_half_sec: ;(clock at 16MHz)
+    ldi  r18, 3;41
+    ldi  r19, 3;150
+    ldi  r20, 5;128
+L1: dec  r20
+    brne L1
+    dec  r19
+    brne L1
+    dec  r18
+    brne L1
+ret
+
  #endif
+
